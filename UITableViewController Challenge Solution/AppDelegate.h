60325ba0aea9b065cf39b155739c24f33d8fdf95
Ernesto Loynaz <eloynaz@Ernestos-MacBook-Pro.local>
2014-10-05 18:40:01 -0400

    first commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Ernesto Loynaz on 10/5/14.
+//  Copyright (c) 2014 Ernesto Loynaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/EFLTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EFLTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Ernesto Loynaz on 10/5/14.
+//  Copyright (c) 2014 Ernesto Loynaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EFLTableViewController : UITableViewController
+
+@end
